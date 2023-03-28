a0e17081068bfd42d4aa4c3a171a7f037bcccc8b
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-03-28 21:31:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+function wrapAdjective(string){
+    const adjective="a hard worker";
+    return `${string} ${adjective} ${string}`;
+}
+function wrapAdjective(char){
+    return function(char2="special"){
+return `You are ${char}${char2}${char}!`
+    }
+}
